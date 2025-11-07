993c295c1e946b3ac655cb27aa063e665081c2df
Phung Ngoc Ha <25021526@vnu.edu.vn>
2025-11-08 02:34:54 +0700

    bai 2

--- homework01.py
+++ homework01.py
@@ -1,2 +1,11 @@
-n = int(input("Nhập vào số nguyên n: "))
+n = int(input("Nhập vào số nguyên n:"))
 print("Số gấp đôi của n là", 2 * n)
+# --- IGNORE ---
+import math
+a = float(input("Nhập vào số thực a: "))
+b = float(input("Nhập vào số thực b: "))
+r = float(input("Nhập vào số thực r: "))
+diện_tích_hình_chữ_nhật = a * b
+diện_tích_hình_tròn = math.pi * r**2
+diện_tích_còn_lại = diện_tích_hình_chữ_nhật - diện_tích_hình_tròn
+print("Diện tích còn lại là", diện_tích_còn_lại)
