d5ad82649857fa1b351955cb7681cd68316bf17e
Phung Ngoc Ha <25021526@vnu.edu.vn>
2025-11-12 03:01:27 +0700

    nop bai tap

--- homework01.py
+++ homework01.py
@@ -44,11 +44,32 @@ else:
     print("Khong phai 3 canh cua tam giac.")
 # --- BAI 7 ---
 s = input ("Nhap vao chuoi ki tu thuong (do dai >= 20:")
-if len(s) >= 20 
+if len(s) >= 20:
     print ("Chu cai thu 5 la ", s[4])
     print ("Chu cai thu 10 den chu cai thu 9 la: ", s[8])
 else: 
     print ("Chuoi qua ngan, phai co do dai >= 20")
 # --- BAI 8 ---
+ten = input("Nhap vao ten cua chu ho: ")
+chi_so_truoc  = int(input("Chi só thang truoc: "))
+chi_so_nay = int(input("Chi so thang nay: "))
+so_dien_tieu_thu = chi_so_nay - chi_so_truoc
+tien_dien = 0
+if so_dien_tieu_thu <= 50:
+    tien_dien = so_dien_tieu_thu * 1984
+elif so_dien_tieu_thu <= 100:
+    tien_dien = (50 * 1984) + (so_dien_tieu_thu - 50) * 2050
+elif so_dien_tieu_thu <= 200:
+    tien_dien = (50 * 1984) + (50 * 2050) + (so_dien_tieu_thu - 100) * 2380
+elif so_dien_tieu_thu <= 300:
+    tien_dien = (50 * 1984) + (50 * 2050) + (100 * 2380) + (so_dien_tieu_thu - 200) * 2998
+elif so_dien_tieu_thu <= 400:
+    tien_dien = (50 * 1984) + (50 * 2050) + (100 * 2380) + (100 * 2998) + (so_dien_tieu_thu - 300) * 3350
+elif so_dien_tieu_thu > 400:
+    tien_dien = (50 * 1984) + (50 * 2050) + (100 * 2380) + (100 * 2998) + (100 * 3350) + (so_dien_tieu_thu - 400) * 3460
+tien_VAT = round(tien_dien * 0.08)
+print ("Ten chu ho: ", ten)
+print ("Tien dien phai tra: ", tien_VAT)
+
 
 
