34b1fc74ed40d50e19f8fa09eb1e80beec2c03f0
Phung Ngoc Ha <25021526@vnu.edu.vn>
2025-11-12 02:48:22 +0700

    bai 7

--- homework01.py
+++ homework01.py
@@ -23,3 +23,32 @@ if c.isalpha():
 else:
     print("c không phải ký tự chữ cái")
 # --- BAI 5 ---
+c = input("Nhập vào chu cai in hoa: ")
+if len(c) == 1 and 'A' <= c <= 'Z':
+    if c == 'A':
+        print ("Khong co chu cai thuong truoc 'a'.")
+    else:
+        chu_cai_thuong_truoc = chr(ord(c) + 32 - 1)
+        print("Chu cai thuong truoc", c.lower(), "la", chu_cai_thuong_truoc)
+else:       
+    print("Vui long nhap mot chu cai in hoa hop le.")
+# --- BAI 6 ---
+a = float(input("Nhập vào cạnh a: "))
+b = float(input("Nhập vào cạnh b: "))
+c = float(input("Nhập vào cạnh c: "))
+if a + b > c and a + c > b and b + c > a:
+    p = (a + b + c ) / 2
+    s = math.sqrt(p * (p-a) * (p - b) * (p-c))
+    print("Ba cạnh trên có thể tạo thành một tam giác. Diện tích tam giác S = {s}")
+else:
+    print("Khong phai 3 canh cua tam giac.")
+# --- BAI 7 ---
+s = input ("Nhap vao chuoi ki tu thuong (do dai >= 20:")
+if len(s) >= 20 
+    print ("Chu cai thu 5 la ", s[4])
+    print ("Chu cai thu 10 den chu cai thu 9 la: ", s[8])
+else: 
+    print ("Chuoi qua ngan, phai co do dai >= 20")
+# --- BAI 8 ---
+
+
