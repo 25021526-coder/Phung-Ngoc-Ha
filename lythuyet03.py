5f638a730b3e6869c32fe3b7398567f5e7fabf30
Phung Ngoc Ha <25021526@vnu.edu.vn>
2025-11-12 14:49:09 +0700

    1

---
+++ lythuyet03.py
@@ -0,0 +1,37 @@
+x = [1,2]
+y = [10,2.5]
+s = x[0] * y[0] + x[1] * y[1]
+print ("tong_tien: ", s)
+# --VD 2--
+numbers = {10, 9, 8, 7, 6, 20, 15}
+for x in numbers:
+    if x % 3 == 0:
+       print ("even")
+    else:
+         print ("odd")
+# --VD 3--
+n = 42
+f = 3.14
+s = "Hello, World!"
+li = [1, 2, 3]
+d = {'key': 'value'}
+bool = True
+print(type(n))
+print(type(f))  
+print(type(s))
+print(type(li))
+print(type(d))
+print (type(bool))
+if bool == True:
+# --VD 4--
+s = "10"
+n = int(s)
+cnt = n + 5
+f = float(cnt)
+age = 25
+s2 = str(age)
+print (n)
+print (f)
+print (s2)
+
+            
